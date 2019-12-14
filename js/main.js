41a96d70119200332c760ba2ad85610119a6ad70
Myrkvithr <place_of_meeting@inbox.ru>
2019-12-14 17:07:17 +0300

    Доделал верстку десктопной версии

--- js/main.js
+++ js/main.js
@@ -17,10 +17,6 @@ $(document).ready(function () {
 
 
     /* Слайдер */
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
+
 
 });
---
+++ js/script.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+    $('#lightSlider').lightSlider({
+        item:3,
+        slideMove: 3,
+        speed: 500
+
+
+        // // responsive [
+        //     {
+        //         breakpoint:800,
+        //         setting: {
+
+        //         }
+        //     },
+
+        //     {
+        //         breakpoint:400,
+        //         setting: {
+
+        //         }
+        //     },
+
+        //     {
+        //         breakpoint:800,
+        //         setting: {
+
+        //         }
+        //     },
+
+        // ]
+
+    });
+
+});
