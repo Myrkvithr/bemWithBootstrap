4356a5a209fa7f97ab8a843b55243dcd9e85198e
Myrkvithr <place_of_meeting@inbox.ru>
2019-12-23 00:53:13 +0300

    допилил адаптацию

--- js/main.js
+++ js/main.js
@@ -16,7 +16,43 @@ $(document).ready(function () {
     });
 
 
-    /* Слайдер */
+    /* Меню */
+    $('ul.menu a[href^="#"').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({'color':'#212121'});
+        $(this).css({'color':'#004bee'});
+        return false;
+    })
+
+/* Выпадающее меню */
+$('.menu-icon').click(function(){
+    $('nav').slideToggle(500);
+    $('ul.menu').css({
+        'display':'flex', 'flex-direction':'column'
+    });
+    if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
+        $(this).html('<i class="fas fa-times"></i>')
+    } else {
+        $(this).html('<i class="fas fa-bars"></i>')
+    }
+})
 
 
+
+
+    /* Кнопка наверх */
+    $(window).scroll(function() {
+        if ($(this).scrollTop() != 0)
+            $('#toTop').fadeIn();
+        else
+            $('#toTop').fadeOut();
+    });
+    $('#toTop').click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+    });
 });
--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     $('#lightSlider').lightSlider({
-        item:3,
+        item: 3,
         slideMove: 3,
-        speed: 500
+        speed: 500,
 
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    item: 2,
+                    slideMove: 2,
 
-        // // responsive [
-        //     {
-        //         breakpoint:800,
-        //         setting: {
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    item: 2,
+                    slideMove: 2,
+                    slideMargin: 6,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    item: 1,
+                    slideMove: 1,
+                }
+            }
+        ]
 
-        //         }
-        //     },
-
-        //     {
-        //         breakpoint:400,
-        //         setting: {
-
-        //         }
-        //     },
-
-        //     {
-        //         breakpoint:800,
-        //         setting: {
-
-        //         }
-        //     },
-
-        // ]
 
     });
 
